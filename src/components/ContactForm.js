eae0d868fb91baae28b68d04db19e5c43bd5d08f
l1nes8 <egor1glavatyje@gmail.com>
2023-10-08 21:28:01 +0300

    update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import css from '../form.module.css';
 
 export default function ContactForm({ contacts, onAddContact }) {
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export default function Filter({ filter, onFilterChange }) {
   const handleChange = e => {
